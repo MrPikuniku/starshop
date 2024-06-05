c7f41c1eb76e53b4d9a17f820d4899f5dbe124ab
Mamouro <sangaremoon@gmail.com>
2024-06-05 23:36:16 +0200

    Utilisation de Twig

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,12 +4,26 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class MainController
+class MainController extends  AbstractController
 {
     #[Route('/')]
-    public function HomePage()
+    public function HomePage() : Response
     {
-        return new Response('<strong>Hello World !</strong>');
+        $starshipCount = 457;
+        $myShip =[
+            'name' => 'USS LeafyCruiser (NCC-0001)',
+            'class' => 'Garden',
+            'captain' => 'Jean-Luc Pickles',
+            'status' => 'under constrution',
+        ];
+
+
+
+        return $this->render("main/homepage.html.twig", [
+            'numberOfStarships' => $starshipCount,
+            'myShip'=> $myShip, 
+        ]);
     }
 }
